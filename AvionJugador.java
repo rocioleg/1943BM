9d4fef78fc1250f185e1cdc252038ce8f92c8af9
roleg <114748499+rocioleg@users.noreply.github.com>
2023-05-18 21:08:23 -0300

    Main+Completo
    Utilizando más clases propias del juego

---
+++ AvionJugador.java
@@ -0,0 +1,59 @@
+import java.awt.*;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class AvionJugador extends VehiculoGuerra {
+    BufferedImage imagen=null;
+    Point2D.Double posicion  = new Point2D.Double();
+    final double speed = 150.0;
+
+    public AvionJugador(String filename, BufferedImage imagen) {
+        super(filename);
+        this.imagen = imagen;
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        g.drawImage(imagen,(int)posicion.getX(),(int)posicion.getY(),null);
+    }
+
+    @Override
+    public double getX() {
+        return posicion.getX();
+    }
+
+    @Override
+    public double getY() {
+        return posicion.getY();
+    }
+
+    @Override
+    public void setImagen(BufferedImage img) {
+        this.imagen=img;
+    }
+
+    @Override
+    public void setPosicion(double x, double y) {
+        posicion.setLocation(x, y);
+    }
+
+    @Override
+    public void setX(double x) {
+        posicion.x=x;
+    }
+
+    @Override
+    public void setY(double y) {
+        posicion.y=y;
+    }
+
+    @Override
+    public void update(double delta) {
+                 
+    }
+
+}
--- VehiculoGuerra.java
+++ VehiculoGuerra.java
@@ -1,9 +1,26 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
 public abstract class VehiculoGuerra extends ObjetoGrafico implements ObjetoMovible{
-
     
-
     public VehiculoGuerra(String filename) {
         super(filename);
     }
+
+    public abstract void setImagen(BufferedImage img);
+
+    public abstract void setPosicion(double x, double y);
+
+    public abstract void setX(double x);    
+
+    public abstract void setY(double y);
+    
+    public abstract double getX();
+
+    public abstract double getY();
+
+    public abstract void update(double delta);
+
+    public abstract void draw(Graphics2D g);
     
 }
